3f68954a76388128795012f80a011a0f4c0215aa
Duncan Rout <duncanrout@yahoo.com>
2024-01-25 15:09:36 -0600

    added doc link

--- components/ApiExample.tsx
+++ components/ApiExample.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Text } from '../components/Themed';
 import { parseString } from 'react-native-xml2js';
 
+// https://www.transitchicago.com/developers/ttdocs/ <-- docs
+
 export default function ApiExample() {
   const apiUrl = 'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=a29c1bad63084bbcaf5ae6a47609150e&mapid=40380';
   const [data, setData] = useState(null);
