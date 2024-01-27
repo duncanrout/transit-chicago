e11f6153ad6796db53df41acb2e610911b0fc2ae
Joe Rutkowski <jbrutkowski77@gmail.com>
2024-01-26 19:39:07 -0600

    working on UI: Experimenting with getting padding between the two stations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,46 +2,102 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 const DATA = [
   {
     id: 'firstItem1',
     title: 'First Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'secondItem1',
     title: 'Second Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'thirdItem1',
     title: 'Third Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'firstItem2',
     title: 'First Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'secondItem2',
     title: 'Second Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'thirdItem2',
     title: 'Third Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'firstItem3',
     title: 'First Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'secondItem3',
     title: 'Second Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
   {
     id: 'thirdItem3',
+    title: 'Th Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'firstItem4',
+    title: 'First Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'secondItem4',
+    title: 'Second Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'thirdItem4',
     title: 'Third Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'firstItem5',
+    title: 'First Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'secondItem5',
+    title: 'Second Item',
+    line: 'The Station',
+    until: 28+" mins",
+  },
+  {
+    id: 'thirdItem5',
+    title: 'Third Item',
+    line: 'The Station',
+    until: 28+" mins",
   },
 ];
-type ItemProps = {title: string};
+type ItemProps = {line: string,
+                  until: string,
+                  title: string};
 
 const Item = ({title}: ItemProps) => (
   <View style={styles.item}>
@@ -54,11 +110,16 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Nearest Stations</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <FlatList 
-      data={DATA}
-      renderItem={({item}) => <Item title={item.title} />}/> 
-      <FlatList data={undefined} renderItem={undefined}/>
-      
+        <ScrollView contentContainerStyle={{padding:5, borderWidth:1, borderColor:'black'}}>
+          <FlatList 
+            data={DATA}
+            renderItem={({item}) => <Item title={item.title} until={item.until} line={item.line}/>}/> 
+        </ScrollView>
+        <ScrollView contentContainerStyle={{padding:5, borderWidth:1, borderColor:'black'}}>
+          <FlatList 
+            data={DATA} 
+            renderItem={({item}) => <Item title={item.title} until={item.until} line={item.line}/>}/>
+        </ScrollView>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -80,9 +141,14 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   item: {
-
+    borderColor:'black',
+    borderWidth:1,
+    width:800,
   },
   timeUntil:{
 
-  }
+  },
+  flatlist: {
+    padding:5
+  },
 });
