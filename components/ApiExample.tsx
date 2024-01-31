8fd3621890cd67d09c264c95c5e86c74c6d7f6db
Roland Rondez <rondezro@gmail.com>
2024-01-31 15:06:07 -0600

    API Returns Data in Arrays

--- components/ApiExample.tsx
+++ components/ApiExample.tsx
@@ -31,7 +31,45 @@ export default function ApiExample() {
   }, [apiUrl]); // Include apiUrl in the dependency array to prevent unnecessary requests
 
   return (
-    <Text>{data && <Text>{JSON.stringify(data, null, 2)}</Text>}</Text>
+    <Text>
+      {data && (
+        <>
+          <Text>Last Updated: {data.ctatt.tmst[0]}</Text>
+          <Text>Error Code: {data.ctatt.errCd[0]}</Text>
+          <Text>Error Name: {data.ctatt.errNm[0]}</Text>
+  
+          <Text>Train Information:</Text>
+          {data.ctatt.eta.map((train, index) => (
+            <Text key={index}>
+              {[
+                `Station ID: ${train.staId[0]}`,
+                `Stop ID: ${train.stpId[0]}`,
+                `Station Name: ${train.staNm[0]}`,
+                `Stop Description: ${train.stpDe[0]}`,
+                `Route: ${train.rt[0]}`,
+                `Destination Station ID: ${train.destSt[0]}`,
+                `Destination Name: ${train.destNm[0]}`,
+                `Train Direction: ${train.trDr[0]}`,
+                `Predicted Departure Time: ${train.prdt[0]}`,
+                `Predicted Arrival Time: ${train.arrT[0]}`,
+                `Is Approaching: ${train.isApp[0]}`,
+                `Is Scheduled: ${train.isSch[0]}`,
+                `Is Delayed: ${train.isDly[0]}`,
+                `Is Flagged: ${train.isFlt[0]}`,
+                `Latitude: ${train.lat[0]}`,
+                `Longitude: ${train.lon[0]}`,
+                `Heading: ${train.heading[0]}`,
+              ].map((info, i) => (
+                <React.Fragment key={i}>
+                  <Text>{info}</Text>
+                  <Text>{'\n'}</Text> {/* Line break */}
+                </React.Fragment>
+              ))}
+            </Text>
+          ))}
+        </>
+      )}
+    </Text>
   );
 }
 
